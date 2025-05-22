218b7348b068e47ef81d068f55698f3958782853
kunal09062004 <116184950+kunal09062004@users.noreply.github.com>
2025-05-22 11:47:35 +0000

    1+2

--- basic/conversionoperations.js
+++ basic/conversionoperations.js
@@ -4,17 +4,16 @@ let score = "33"
 // console.log(typeof(score));
 
 let valueInNumber = Number(score)
-// console.log(typeof (valueInNumber));
-// console.log(valueInNumber);
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);
 
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = 1
-
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let isLoggedIn = 1
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(BooleanisLoggedIn)
 
 // 1 => true; 0 => false  
@@ -24,5 +23,34 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************Opertations************************* //
+
+
+let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " kunal"
+
+let str3 = str2 + str1
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
