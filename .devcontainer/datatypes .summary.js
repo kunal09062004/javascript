feaf9474c4f1e2002f6b6eec35e2b2b1daeba17b
kunal09062004 <116184950+kunal09062004@users.noreply.github.com>
2025-05-23 11:52:48 +0000

    about datatypes

--- .devcontainer/datatypes .summary.js
+++ .devcontainer/datatypes .summary.js
@@ -3,7 +3,7 @@
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt.
 
 const score = 100
-const score = 100.3
+const scoree = 100.3
 
 const isLoggedIn = false
 const outsideTemp = null
@@ -12,7 +12,45 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123') 
 
-console.log(Id === anotherId)
+console.log(id === anotherId)
+
 // Reference type(non primitive)
 
 // Array, Object, Functions
+
+
+const heros = ["shaktiman, naagraj"]
+const obj ={
+    name: "kunal",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("Hello world")
+}
+
+console.log(typeof bigNumber);
+
+// ========================
+
+// Stack (Primitive), Heap(non-Primitive)
+
+let myYoutubename = "kunalbaby"
+
+let anotherName = myYoutubename
+anotherName = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+    email: "usergoogle@.com",
+    upi: "user@byl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "kunal@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
